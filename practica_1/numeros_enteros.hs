9f265a0b21defa97d970415de710911c08c7e0a6
LautaroLovato <lautaro.lovato07@gmail.com>
2024-08-13 20:45:45 -0300

    numeros enteros

---
+++ practica_1/numeros_enteros.hs
@@ -0,0 +1,26 @@
+-- parte 1
+
+sucesor :: Int -> Int
+sucesor n = n+1
+
+sumar :: Int -> Int -> Int
+sumar n m = n+m
+
+divisionYResto :: Int -> Int -> (Int, Int)
+divisionYResto n m = ( div n  m , mod n m )
+
+maxDelPar :: (Int, Int) -> Int
+maxDelPar (n, m) = if (n > m)
+                        then n
+                        else m
+
+-- parte 2
+
+{-
+De 4 ejemplos de expresiones diferentes que denoten el número 10, 
+utilizando en cada expresión a todas las funciones del punto anterior.
+1. maxDelPar (divisionYResto (sucesor 19) (sumar 1 1) )
+2. maxDelPar (divisionYResto (sumar 15 5) (sucesor 1) )
+3. maxDelPar (divisionYResto (sumar 1 9) (sucesor 0 ) )
+4. maxDelPar (divisionYResto (sumar 3 7 ) (sucesor 23 ) )
+-}
