bed146ad7fee5b93253fceba1b8fb9d3e720124e
LautaroLovato <lautaro.lovato07@gmail.com>
2024-08-16 19:26:28 -0300

    Pattern matching sobre listas

--- practica_1/patternMatching_sobre_listas.hs
+++ practica_1/patternMatching_sobre_listas.hs
@@ -4,3 +4,10 @@ estaVacia _ = False
 
 elPrimero :: [a] -> a
 elPrimero (x:_) = x
+
+sinElPrimero :: [a] -> [a]
+sinElPrimero (_:xs) = xs
+
+-- precon: la lista no debe ser vacia 
+splitHead :: [a] -> (a, [a])
+splitHead xs = (elPrimero xs, sinElPrimero xs)
