bd7d84ae4b534dfabd47688388459d4e6ddc41aa
LautaroLovato <lautaro.lovato07@gmail.com>
2024-08-21 17:34:26 -0300

    correccion estructura del proyecto

---
+++ practica_01.hs
@@ -0,0 +1,229 @@
+-- Numeros Enteros
+-- punto 1
+
+sucesor :: Int -> Int
+sucesor n = n+1
+
+sumar :: Int -> Int -> Int
+sumar n m = n+m
+
+divisionYResto :: Int -> Int -> (Int, Int)
+divisionYResto n m = ( div n  m , mod n m )
+
+maxDelPar :: (Int, Int) -> Int
+maxDelPar (n, m) = if (n > m)
+                        then n
+                        else m
+
+-- punto 2
+
+{-
+De 4 ejemplos de expresiones diferentes que denoten el número 10, 
+utilizando en cada expresión a todas las funciones del punto anterior.-}
+1. maxDelPar (divisionYResto (sucesor 19) (sumar 1 1) )
+2. maxDelPar (divisionYResto (sumar 15 5) (sucesor 1) )
+3. maxDelPar (divisionYResto (sumar 1 9) (sucesor 0 ) )
+4. maxDelPar (divisionYResto (sumar 3 7 ) (sucesor 23 ) )
+
+
+-- Tipos enumerativos
+
+-- punto 1 
+
+data Dir = Norte | Este | Sur | Oeste
+    deriving Show
+
+opuesto :: Dir -> Dir 
+opuesto Norte = Sur
+opuesto Este = Oeste
+opuesto Sur = Norte 
+opuesto Oeste = Este
+
+iguales :: Dir -> Dir -> Bool
+iguales Norte Norte = True 
+iguales Este Este   = True
+iguales Sur Sur     = True 
+iguales Oeste Oeste = True
+iguales _ _         = False
+
+-- punto 2
+
+-- creo el tipo DiaDeSemana
+data DiaDeSemana = Lunes | Martes | Miercoles | Jueves | Viernes | Sabado | Domingo
+    deriving Show
+
+primeroYUltimoDia :: (DiaDeSemana, DiaDeSemana)
+primeroYUltimoDia = (Lunes, Domingo)
+
+empiezaConM :: DiaDeSemana -> Bool
+empiezaConM Martes = True
+empiezaConM Miercoles = True
+empiezaConM _ = False
+
+vieneDespues :: DiaDeSemana -> DiaDeSemana -> Bool
+vieneDespues d1 d2 = numeroDia d1 > numeroDia d2
+
+numeroDia :: DiaDeSemana -> Int
+numeroDia Lunes     = 1
+numeroDia Martes    = 2
+numeroDia Miercoles = 3
+numeroDia Jueves    = 4
+numeroDia Viernes   = 5
+numeroDia Sabado    = 6
+numeroDia Domingo   = 7
+
+estaEnElMedio :: DiaDeSemana -> Bool
+estaEnElMedio Lunes = False
+estaEnElMedio Domingo = False
+estaEnElMedio _ = True  
+
+-- punto 3
+
+negar :: Bool -> Bool
+negar True = False
+negar False = True
+
+implica :: Bool -> Bool -> Bool
+implica True False = False
+implica _ _ = True
+
+yTambien :: Bool -> Bool -> Bool
+yTambien True True = True 
+yTambien _ _  = False
+
+oBien :: Bool -> Bool -> Bool
+oBien True _ = True
+oBien _ True = True 
+oBien _ _ = False
+
+
+--Registros
+
+-- punto 1
+
+data Persona = P String Int 
+            -- Nombre Edad 
+    deriving Show
+
+lautaro = P "lautaro" 21
+
+nombre :: Persona -> String 
+nombre (P n e ) = n
+
+edad :: Persona -> Int
+edad (P n e ) = e
+
+crecer :: Persona -> Persona 
+crecer (P n e )= P n (e+ 1) 
+
+cambioDeNombre :: String -> Persona -> Persona
+cambioDeNombre nuevoNombre (P n e ) = (P nuevoNombre e )
+
+esMayorQueLaOtra :: Persona -> Persona -> Bool
+esMayorQueLaOtra (P nX eX ) (P nY eY ) = eX > eY
+
+laQueEsMayor :: Persona -> Persona -> Persona
+-- precon: debe haber una persona mayor a la otra para su correcto funcionamiento
+laQueEsMayor (P nX eX ) (P nY eY ) = if (eX > eY )
+                                        then (P nX eX )
+                                        else (P nY eY )
+
+-- punto 2
+
+data Pokemon = Pok TipoDePokemon Int
+            -- Tipo PorcentajeDeEnergia 
+    deriving Show
+
+data TipoDePokemon = Agua | Fuego | Planta
+    deriving Show
+
+
+data Entrenador = E String Pokemon Pokemon
+             -- Nombre Pokemon1 Pokemon2
+    deriving Show
+
+charizard = Pok Fuego 10
+venasaur = Pok Planta 5
+entrenador = E "lautaro" charizard venasaur
+
+superaA :: Pokemon -> Pokemon -> Bool
+superaA (Pok tX pX) (Pok tY pY) = if(esTipoSuperior tX tY)
+                                    then True
+                                    else False
+
+-- indica si el primer tipo de pokemon es superior al segundo
+esTipoSuperior :: TipoDePokemon -> TipoDePokemon -> Bool
+esTipoSuperior Agua Fuego = True
+esTipoSuperior Fuego Planta = True
+esTipoSuperior Planta Agua  = True
+esTipoSuperior _ _ = False
+
+
+cantidadDePokemonDe :: TipoDePokemon -> Entrenador -> Int
+cantidadDePokemonDe t (E n p1 p2) = unoSiCeroSino (esDeTipo p1 t) + unoSiCeroSino (esDeTipo p2 t)
+
+unoSiCeroSino:: Bool -> Int
+unoSiCeroSino b = if(b)
+                    then 1
+                    else 0
+                    
+esDeTipo :: Pokemon -> TipoDePokemon -> Bool
+esDeTipo p Agua   = esDeAgua p
+esDeTipo p Fuego = esDeFuego p
+esDeTipo p Planta = esDePlanta p 
+esDeTipo _ _ = False
+
+-- indica si el pokemon es de tipo agua 
+esDeAgua :: Pokemon -> Bool
+esDeAgua (Pok Agua _) = True 
+esDeAgua (Pok _ _) = False
+
+-- indica si el pokemon es de tipo fuego 
+esDeFuego :: Pokemon -> Bool
+esDeFuego (Pok Fuego _) = True 
+esDeFuego (Pok _ _) = False
+
+-- indica si el pokemon es de tipo planta  
+esDePlanta :: Pokemon -> Bool
+esDePlanta (Pok Planta _) = True 
+esDePlanta (Pok _ _) = False
+
+
+juntarPokemon :: (Entrenador, Entrenador) -> [Pokemon]
+juntarPokemon ((E nX p1 p2 ), (E nY p3 p4 )) = [p1, p2, p3, p4]
+
+
+--Funciones polimorficas
+
+-- punto 1
+
+loMismo :: a -> a
+loMismo a = a
+
+siempreSiete :: a -> Int
+siempreSiete a = 7
+
+swap :: (a,b) -> (b, a)
+swap (a,b) = (b,a)
+
+{-
+punto 2 
+Estas funciones son polimorficas porque tienen sentido para más de un tipo
+-}
+
+
+-- Pattern Matching sobre listas
+
+estaVacia :: [a] -> Bool
+estaVacia [] = True
+estaVacia _ = False
+
+elPrimero :: [a] -> a
+elPrimero (x:_) = x
+
+sinElPrimero :: [a] -> [a]
+sinElPrimero (_:xs) = xs
+
+-- precon: la lista no debe ser vacia 
+splitHead :: [a] -> (a, [a])
+splitHead xs = (elPrimero xs, sinElPrimero xs)
--- practica_02.hs
+++ practica_02.hs
@@ -1,3 +1,113 @@
+-- Punto 1
+
+sumatoria :: [Int] -> Int 
+sumatoria [] = 0
+sumatoria (n:ns) = n + sumatoria ns 
+
+longitud :: [a] -> Int
+longitud [] = 0
+longitud (n:ns) = 1 + longitud ns
+
+sucesores :: [Int] -> [Int]
+sucesores [] = []
+sucesores (n:ns) = n + 1 : sucesores ns
+
+conjuncion :: [Bool] -> Bool
+conjuncion [] = False
+conjuncion (False:_) =  False
+conjuncion (True:[]) = True
+conjuncion (True:xs) = conjuncion xs
+
+disyuncion :: [Bool] -> Bool
+disyuncion [] = False
+disyuncion (True:_) = True 
+disyuncion (False:xs) = disyuncion xs
+
+
+aplanar :: [[a]] -> [a]
+aplanar [] = []
+aplanar (x:xs) =  agregar x  (aplanar xs)
+
+pertenece :: Eq a => a -> [a] -> Bool
+pertenece e [] = False
+pertenece e (x:xs) = e == x || pertenece e xs
+
+apariciones :: Eq a => a -> [a] -> Int
+apariciones e [] = 0
+apariciones e (x:xs) =  unoSiCeroSino (e == x) + apariciones e xs
+
+unoSiCeroSino:: Bool -> Int
+unoSiCeroSino b = if(b)
+                    then 1
+                    else 0
+
+losMenoresA :: Int -> [Int] -> [Int]
+losMenoresA _ [] = [] 
+losMenoresA k (n:ns) = if n < k
+                          then n : losMenoresA k ns
+                          else losMenoresA k ns
+
+lasDeLongitudMayorA :: Int -> [[a]] -> [[a]]
+lasDeLongitudMayorA _ [] = []
+lasDeLongitudMayorA n (x:xs) = if length x > n
+                                    then x : lasDeLongitudMayorA n xs
+                                    else lasDeLongitudMayorA n xs 
+
+
+agregarAlFinal :: [a] -> a -> [a]
+agregarAlFinal xs y = xs ++ [y]
+
+agregar :: [a] -> [a] -> [a]
+agregar [] ys = ys
+agregar (x:xs) ys =  x : agregar xs ys
+
+reversa :: [a] -> [a]
+reversa [] = []
+reversa (x:xs) = reversa xs ++ [x]
+
+zipMaximos :: [Int] -> [Int] -> [Int]
+zipMaximos _ [] = []
+zipMaximos [] _ = []
+zipMaximos (x:xs) (y:ys) = if x > y 
+                                then x : zipMaximos xs ys 
+                                else y : zipMaximos xs ys
+
+elMinimo :: Ord a => [a] -> a
+--Precon: La lista no debe ser vacia 
+elMinimo (x:[]) = x
+elMinimo (x:xs) = if x < elMinimo xs
+                        then x
+                        else elMinimo xs
+
+-- Punto 2 
+factorial :: Int -> Int 
+-- precond: el numero debe ser mayor o igual a cero
+factorial 0 = 1
+factorial n = n * factorial (n-1)
+
+cuentaRegresiva :: Int -> [Int]
+cuentaRegresiva 0 = []
+cuentaRegresiva n = n : cuentaRegresiva (n-1)
+
+repetir :: Int -> a -> [a]
+--precond: el numero debe ser mayor o igual a 1
+repetir 0 _ = []
+repetir n x = x : repetir (n-1) x
+ 
+
+losPrimeros :: Int -> [a] -> [a]
+-- precond: el numero debe ser mayor o igual a 1
+losPrimeros 0 _  = []
+losPrimeros _ [] = []
+losPrimeros n (x:xs) = x : losPrimeros (n-1) xs 
+
+sinLosPrimeros :: Int -> [a] -> [a]
+-- precon: el numero debe ser mayor o igual a cero, y la lista debe tener al menos n elementos 
+sinLosPrimeros 0 x = x
+sinLosPrimeros n (x:xs) = sinLosPrimeros (n-1) xs
+
+-- Punto 3
+
 data Persona = P String Int 
               -- Nombre Edad 
     deriving Show
@@ -113,3 +223,32 @@ hayPokemonDeTipo :: [Pokemon] -> TipoDePokemon -> Bool
 -- dada una lista de pokemons, indica si hay al menos un pokemon del tipo dado
 hayPokemonDeTipo [] _ = False
 hayPokemonDeTipo (x:xs) t = esDeTipo x t || hayPokemonDeTipo xs t  
+
+
+data Seniority = Junior | SemiSenior | Senior
+    deriving Show
+data Proyecto = ConsProyecto String
+    deriving Show
+data Rol = Developer Seniority Proyecto | Management Seniority Proyecto
+    deriving Show
+data Empresa = ConsEmpresa [Rol]
+    deriving Show
+
+proyecto1 = ConsProyecto "p1"
+proyecto2 = ConsProyecto "p2"
+
+junior = Developer Junior proyecto1
+semiSenior = Developer SemiSenior proyecto1
+senior = Management Senior proyecto2
+
+empresa = ConsEmpresa [junior, semiSenior, senior]
+
+proyectos :: Empresa -> [Proyecto]
+--Dada una empresa denota la lista de proyectos en los que trabaja, sin elementos repetidos.
+proyectos (ConsEmpresa []) = []
+proyectos (ConsEmpresa (x:xs)) = proyecto x : proyectos (ConsEmpresa xs)
+
+
+proyecto :: Rol -> Proyecto
+proyecto (Developer _ p) = p
+proyecto (Management _ p) = p
