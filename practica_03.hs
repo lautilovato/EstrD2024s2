ab5f617e612ff162c87e7199e90fb25db109e568
LautaroLovato <lautaro.lovato07@gmail.com>
2024-08-26 22:42:46 -0300

    tipos recursivos simples practica 3

---
+++ practica_03.hs
@@ -0,0 +1,98 @@
+-- Punto 1.1
+
+data Color = Azul | Rojo
+    deriving Show
+data Celda = Bolita Color Celda | CeldaVacia
+    deriving Show
+
+nroBolitas :: Color -> Celda -> Int
+{-Dados un color y una celda, indica la cantidad de bolitas de ese color. 
+Nota: pensar si ya existe una operación sobre listas que ayude a resolver el problema-}
+nroBolitas _ CeldaVacia = 0
+nroBolitas colX (Bolita colY cel) = unoSiCeroSino (esDeColor colY colX) + nroBolitas colX cel
+
+
+unoSiCeroSino:: Bool -> Int
+unoSiCeroSino True = 1
+unoSiCeroSino False = 0
+
+esDeColor :: Color -> Color -> Bool
+esDeColor Azul Azul = True 
+esDeColor Rojo Rojo = True
+esDeColor _ _ = False
+
+celda0 = Bolita Rojo (Bolita Azul (Bolita Rojo CeldaVacia))
+
+poner :: Color -> Celda -> Celda
+poner col cel = (Bolita col cel) 
+
+sacar :: Color -> Celda -> Celda
+sacar _ CeldaVacia = CeldaVacia
+sacar colX (Bolita colY cel) = if esDeColor colX colY
+                                    then cel
+                                    else Bolita colY (sacar colX cel)
+
+ponerN :: Int -> Color -> Celda -> Celda
+ponerN 0 _ c = c
+ponerN n col cel = ponerN (n-1) col (Bolita col cel)
+
+-- Punto 1.2
+
+data Objeto = Cacharro | Tesoro
+    deriving Show
+data Camino = Fin | Cofre [Objeto] Camino | Nada Camino
+    deriving Show
+
+camino1 = Nada (Nada Fin)
+camino2 = Nada (Nada (Cofre [Cacharro] Fin))
+camino3 = Nada (Nada (Cofre [Tesoro,Tesoro] (Nada Fin)))
+
+hayTesoro :: Camino -> Bool
+hayTesoro Fin = False
+hayTesoro (Cofre objs c) = hayTesoroEnObjetos objs || hayTesoro c
+hayTesoro (Nada c)= hayTesoro c
+
+hayTesoroEnObjetos :: [Objeto] -> Bool
+hayTesoroEnObjetos [] = False
+hayTesoroEnObjetos (obj:objs) = esTesoro obj || hayTesoroEnObjetos objs
+
+esTesoro :: Objeto -> Bool
+esTesoro Tesoro = True
+esTesoro _ = False
+
+pasosHastaTesoro :: Camino -> Int
+--precond: existe al menos un tesoro en el camino
+pasosHastaTesoro Fin = error "No se encontro un tesoro"
+pasosHastaTesoro (Cofre objs c) = if hayTesoroEnObjetos objs
+                                        then 0
+                                        else pasosHastaTesoro c
+pasosHastaTesoro (Nada c) = 1 + pasosHastaTesoro c
+
+
+hayTesoroEn :: Int -> Camino -> Bool
+hayTesoroEn _ Fin = False
+hayTesoroEn n (Cofre objs c) = hayTesoroEnObjetos objs && n == 0
+hayTesoroEn n (Nada c) = hayTesoroEn (n-1) c
+
+alMenosNTesoros :: Int -> Camino -> Bool
+alMenosNTesoros n Fin = False
+alMenosNTesoros n (Cofre objs c) = tesorosEnObjetos objs + tesorosEnCamino c >= n 
+alMenosNTesoros n (Nada c) = tesorosEnCamino c >= n
+
+tesorosEnCamino :: Camino -> Int
+-- indica cuantos tesoros hay en el camino
+tesorosEnCamino Fin = 0
+tesorosEnCamino (Cofre objs c) = tesorosEnObjetos objs + tesorosEnCamino c
+tesorosEnCamino (Nada c) = tesorosEnCamino c
+
+tesorosEnObjetos :: [Objeto] -> Int
+tesorosEnObjetos [] = 0
+tesorosEnObjetos (obj:objs) = unoSiCeroSino (esTesoro obj) + tesorosEnObjetos objs 
+
+
+cantTesorosEntre :: Int -> Int -> Camino -> Int
+cantTesorosEntre _ _ Fin = 0
+cantTesorosEntre n m (Cofre objs c) =  if n <= 0 && m >= 0
+                                            then tesorosEnObjetos objs + cantTesorosEntre (n-1) (m-1) c
+                                            else cantTesorosEntre (n-1) (m-1) c
+cantTesorosEntre n m (Nada c) = cantTesorosEntre (n-1) (m-1) c
